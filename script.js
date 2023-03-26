7d1a0ead3d600420f10e80f48f6b60335e84c531
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-03-26 17:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function manipulateData(array) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(array);
+    }, 3000);
+  });
+}
+
+manipulateData([1, 2, 3, 4])
+  .then((array) => {
+    const filteredArray = array.filter((num) => num % 2 === 0);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(filteredArray);
+      }, 1000);
+    });
+  })
+  .then((filteredArray) => {
+    const multipliedArray = filteredArray.map((num) => num * 2);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(multipliedArray);
+      }, 2000);
+    });
+  })
+  .then((multipliedArray) => {
+    const output = document.getElementById("output");
+    output.textContent = multipliedArray.join(", ");
+  });
+
