1bc8ef70a7bd296df17c1f3bbec9090717b64129
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2024-10-15 18:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-function array(arr){
-   return new Promise((resolve,reject)=>{
-            resolve(arr)
-    })
+
+function myPromise(arr){
+	returm new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(arr)
+		},3000)
+	})
 }
 
-let promise1 = (dataFromPromise1)=>{
-   return new Promise((resolve,reject)=>{
-        let odd = []
-        setTimeout(() => {
-            dataFromPromise1.map((res)=>{
-                if(res%2!=0){
-                    odd.push(res)
-                }
-            })
-            resolve(odd)
-        }, 1000);
-    })
+function filter(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let filteredNmbr = []
+			arr.map((item)=>{
+			if(item%2!=0){
+				filteredNmbr.push(item)
+			}
+		})
+		},1000)
+	})
 }
 
-let promise2 = (dataFromPromise1)=>{
-  return  new Promise((resolve,reject)=>{
-        let even = []
-        setTimeout(() => {
-            dataFromPromise1.map((res)=>{
-                if(res%2==0){
-                    even.push(res*2)
-                }
-            })
-            resolve(even)
-        }, 3000);
-    })
+function even(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let evenTowTimes = []
+		arr.map((item)=>{
+			if(item%2==0){
+				evenTowTimes.push(item)
+			}
+		})
+		},2000)
+	})
 }
 
-array([1,2,3,4]).then((res)=>{
-    document.getElementById("output").innerHTML=`<h1>Given Array is>>> [${res}]</h1>`
-   // console.log(res)
-   return promise1(res)
-}).then((res2)=>{
-   // console.log(res2)
-   document.getElementById("output").innerHTML += `<h3>odd elements from the array are >>> [${res2}]</h3>`
-   return promise2([1,2,3,4])
+let array = [1,2,3,4]
+let op = document.getElementById("output").innerHTML
+
+myPromise(array).then((res)=>{
+	return filter(array)
 }).then((res)=>{
-   // console.log(res)
-   document.getElementById("output").innerHTML+=`<h3>Even elements after multiply by 2 from the array are >>> [${res}]</h3>`
-}).catch((err) => {
-    console.error("Error:", err);
-});
+		op = res
+	return even(array)
+}).then((res)=>{
+	op = res
+})
+
+
+
+
+
